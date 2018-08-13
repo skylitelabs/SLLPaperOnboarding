d70480b8e97ab056e1b6601bd267ee8adbca6cc5
Leejay Schmidt <leejay@leejayschmidt.com>
2018-08-13 12:33:57 -0600

    Initial Commit of sample app

---
+++ SLLPaperOnboardingDemo/SLLPaperOnboardingDemo/SLLPaperOnboardingDemo/SLLViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SLLPaperOnboardingDemo
+//
+//  Created by Leejay Schmidt on 2018-08-13.
+//  Copyright © 2018 Skylite Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLLViewController : UIViewController
+
+
+@end
+
---
+++ SLLPaperOnboardingDemo/SLLPaperOnboardingDemo/SLLPaperOnboardingDemo/SLLViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SLLPaperOnboardingDemo
+//
+//  Created by Leejay Schmidt on 2018-08-13.
+//  Copyright © 2018 Skylite Labs. All rights reserved.
+//
+
+#import "SLLViewController.h"
+
+@interface SLLViewController ()
+
+@end
+
+@implementation SLLViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
